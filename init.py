0b71c644792b74a61401d5dadd50fff1dd0b778f
lorenzopalloni <palloni.lorenzo@gmail.com>
2022-08-15 10:50:49 +0200

    Clean minor stuff

--- init.py
+++ init.py
@@ -8,25 +8,38 @@ from argparse import ArgumentParser
 from pathlib import Path
 
 def parse_args():
+    """Argument parser."""
     parser = ArgumentParser()
     parser.add_argument('--input_dir', '-i', default=Path(), type=Path)
     parser.add_argument('--output_dir', '-o', default=Path.home(), type=Path)
     return parser.parse_args()
 
 def copy_safely(src: Path, dst: Path) -> None:
+    """Copy `src` to `dst` asking always for permission.
+    
+    Args:
+        src (Path): source file pathname
+        dst (Path): destination file pathname
+    """
     print(f"I'm going to copy {src} to {dst}.")
     ans = input("May I have your permission? [y/N]")
     trues = ('y', 'yes', '1')
     falses = ('n', 'no', '0', '')
     if ans.lower() in trues:
-        shutil.copyfile(src, dst) 
+        shutil.copyfile(src, dst)
     elif ans.lower() in falses:
         return None
     else:
         return copy_safely(src, dst)
     return None
 
-def move_safely(input_dir: Path, output_dir: Path):
+def copy_all_dotfiles_safely(input_dir: Path, output_dir: Path) -> None:
+    """Copy all dotfiles in `input_dir` to `output_dir`.
+
+    Args:
+        src (Path): source directory pathname
+        dst (Path): destination directory pathname
+    """
     print(f'input_dir: {input_dir}')
     print(f'output_dir: {output_dir}')
 
@@ -42,7 +55,7 @@ def move_safely(input_dir: Path, output_dir: Path):
             and x.name not in excluded
         )
     ]
-    
+
     for i, new in enumerate(all_dotfiles, start=1):
         print(f'[ {i} / {len(all_dotfiles)} ]')
         old = output_dir / new.name
@@ -58,4 +71,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
